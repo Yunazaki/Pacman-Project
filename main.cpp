a40e74119240780d9cd3408bdf1d228a0abeeb21
Yunazaki <66821940+Yunazaki@users.noreply.github.com>
2022-02-24 17:47:29 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <cmath>
 #include "SFML/Graphics.hpp"
 using namespace std;
 
@@ -20,14 +21,14 @@ int main() {
 
     int map[10][24] = {
         1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-        1,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,
-        1,0,1,1,1,0,1,1,0,1,0,1,1,1,0,1,1,1,0,0,0,1,0,1,
-        1,0,1,0,0,0,1,0,0,1,0,0,0,0,0,1,1,0,0,1,0,0,0,1,
-        1,0,0,0,1,1,1,0,1,1,0,1,1,1,0,0,0,0,1,1,0,1,0,1,
-        1,1,0,0,0,1,0,0,0,1,0,1,1,1,1,0,1,0,1,1,0,1,0,1,
-        1,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-        1,0,1,1,1,0,1,1,0,1,0,1,0,1,1,1,0,1,1,1,0,1,0,1,
-        1,0,0,0,0,0,0,0,0,1,0,0,0,1,1,1,0,0,0,0,0,0,0,1,
+        1,0,0,0,1,0,1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,1,
+        1,0,1,0,1,0,1,0,1,1,1,1,0,1,0,1,0,1,0,1,0,1,0,1,
+        1,0,1,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,1,
+        1,0,0,0,0,1,1,0,1,1,0,1,1,0,0,0,1,0,0,0,0,0,1,1,
+        1,1,0,1,0,1,0,0,0,1,0,1,0,0,1,0,1,0,1,1,1,0,1,1,
+        1,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1,
+        1,0,1,1,1,0,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1,0,1,
+        1,0,0,0,0,0,1,1,1,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1,
         1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
     };
 
@@ -36,11 +37,18 @@ int main() {
     int ypos = 85;
     int vx = 0;
     int vy = 0;
-    int radius = 55;
-    sf::CircleShape player(35);
+    int radius = 35;
+    sf::CircleShape player(radius);
     player.setFillColor(sf::Color(250, 250, 0)); //using RGB value for color here (hex also works)
     player.setPosition(xpos, ypos); //top left "corner" of circle (not center!)
     bool keys[] = { false, false, false, false };
+
+    bool collide = false;
+
+    //coin
+    int coinRad = 5;
+    sf::CircleShape coin(coinRad);
+    coin.setFillColor(sf::Color(250, 250, 0));
 
     //################### HOLD ONTO YOUR BUTTS, ITS THE GAME LOOP###############################################################
     while (screen.isOpen()) {//keep window open until user shuts it down
@@ -82,39 +90,76 @@ int main() {
         if (keys[LEFT] == true)
         {
             vx = -5;
+            vy = 0;
         }
         else if (keys[RIGHT] == true)
         {
             vx = 5;
+            vy = 0;
         }
         else if (keys[UP] == true)
         {
             vy = -5;
+            vx = 0;
         }
         else if (keys[DOWN] == true)
         {
             vy = 5;
+            vx = 0;
         }
+        else
+        {
+            
+        }   
         xpos += vx;
         ypos += vy;
         player.setPosition(xpos, ypos);
 
-        if (vx > 0 && map[(ypos) / 80][(xpos + radius * 2 + 3) / 80] == 1)
+        if (vx > 0 && map[(ypos + 5) / 80][(xpos + (radius * 2) + 5) / 80] == 1 || map[(ypos + (radius * 2) - 5) / 80][(xpos + (radius * 2) - 5) / 80] == 1)
         {
             vx = 0;
+        }
+        if (vx > 0 && map[(ypos - 5) / 80][(xpos - (radius / 2) - 10) / 80] == 1 || map[(ypos - 5) / 80][(xpos - (radius / 2) + 10) / 80] == 1)
+        {
+            vx = 0;
+        }
+        if (vy > 0 && map[(ypos + (radius * 2) + 5) / 80][(xpos + 5) / 80] == 1 || map[(ypos + (radius * 2) - 5) / 80][(xpos + (radius * 2) - 5) / 80] == 1)
+        {
+            vy = 0;
+        }
+        if (vy < 0 && map[(ypos + (-radius / 2) + 10) / 80][(xpos) / 80] == 1)
+        {
+            vy = 0;
+        }
+
+        if (map[(ypos + 5) / 80][(xpos + 5) / 80] == 0)
+        {
+            map[(ypos + 5) / 80][(xpos + 5) / 80] = -1;
         }
 
         //render section-----------------------------------------
         screen.clear(); //wipes screen, without this things smear
-        for (int rows = 0; rows < 10; rows++)
+        for (int rows = 0; rows < 10; rows++) {
             for (int cols = 0; cols < 24; cols++) {
+                if (map[rows][cols] == 0)
+                {
+                    coin.setPosition(((cols * 80) + 40), ((rows * 80) + 40));
+                    
+                    screen.draw(coin);
+                }
                 if (map[rows][cols] == 1) {
                     wall.setPosition(cols * 80, rows * 80);
                     screen.draw(wall);
                 }
             }
+        }
         screen.draw(player);
         screen.display(); //flips memory drawings onto screen
+
+        if (collide == true)
+        {
+            return NULL;
+        }
 
     }//######################## end game loop ###################################################################################
 
